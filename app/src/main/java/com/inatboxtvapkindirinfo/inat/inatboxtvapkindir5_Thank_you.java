a57ed1567c4741e7088791ebd48086f1e9392fb0
Savan2909 <133249665+savan2909git@users.noreply.github.com>
2023-07-11 20:18:55 +0530

    final with ad id and link

--- app/src/main/java/com/inatboxtvapkindirinfo/inat/inatboxtvapkindir5_Thank_you.java
+++ app/src/main/java/com/inatboxtvapkindirinfo/inat/inatboxtvapkindir5_Thank_you.java
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.facebook.ads.Ad;
 import com.facebook.ads.AdError;
 import com.facebook.ads.InterstitialAd;
+import com.facebook.ads.InterstitialAdListener;
 import com.facebook.ads.NativeAd;
 import com.facebook.ads.NativeAdListener;
 
@@ -29,6 +30,9 @@ public class inatboxtvapkindir5_Thank_you extends AppCompatActivity {
 
     public void onBackPressed() {
         super.onBackPressed();
+        ShowFullAds();
+        inatboxtvapkindir5_splesh.url_passing(this);
+        inatboxtvapkindir5_splesh.url_passing1(this);
     }
 
     /* access modifiers changed from: protected */
@@ -38,6 +42,10 @@ public class inatboxtvapkindir5_Thank_you extends AppCompatActivity {
 
         loadfbNativeAd();
         showfbbanner();
+        ShowFullAds();
+        inatboxtvapkindir5_splesh.url_passing(this);
+        inatboxtvapkindir5_splesh.url_passing1(this);
+
 
         ((ImageView) findViewById(R.id.visitr)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -229,6 +237,130 @@ public class inatboxtvapkindir5_Thank_you extends AppCompatActivity {
                 bannerAdContainer.buildLoadAdConfig()
                         .withAdListener(nativeAdListener)
                         .build());
+    }
+
+    public void ShowFullAds() {
+        Log.e(TAG, "fbfull1 " + getString(R.string.fbfull));
+        try {
+            if (inatboxtvapkindir5_splesh.interstitialAd1 != null && inatboxtvapkindir5_splesh.interstitialAd1.isAdLoaded())
+                inatboxtvapkindir5_splesh.interstitialAd1.show();
+            else {
+                if (!inatboxtvapkindir5_splesh.interstitialAd1.isAdLoaded())
+                    inatboxtvapkindir5_splesh.interstitialAd1.loadAd();
+
+                interstitialAd = new InterstitialAd(this, getString(R.string.fbfull));
+                InterstitialAdListener interstitialAdListener = new InterstitialAdListener() {
+
+
+                    @Override
+                    public void onInterstitialDisplayed(Ad ad) {
+
+                    }
+
+                    @Override
+                    public void onInterstitialDismissed(Ad ad) {
+                        // Interstitial dismissed callback
+                        Log.e(TAG, "fbfull1 " + "Interstitial ad dismissed.");
+                    }
+
+                    @Override
+                    public void onError(Ad ad, AdError adError) {
+                        // Ad error callback
+                        Log.e(TAG, "fbfull1 " + adError.getErrorMessage());
+                        ShowFullAds1();
+                    }
+
+                    @Override
+                    public void onAdLoaded(Ad ad) {
+                        Log.d(TAG, "fbfull1 " + "Interstitial ad is loaded and ready to be diSplash_screenlayed!");
+                        if (interstitialAd != null && interstitialAd.isAdLoaded())
+                            interstitialAd.show();
+                    }
+
+                    @Override
+                    public void onAdClicked(Ad ad) {
+                        // Ad clicked callback
+                        Log.d(TAG, "fbfull1 " + "Interstitial ad clicked!");
+                    }
+
+                    @Override
+                    public void onLoggingImpression(Ad ad) {
+                        // Ad impression logged callback
+                        Log.d(TAG, "fbfull1 " + "Interstitial ad impression logged!");
+                    }
+                };
+
+                interstitialAd.loadAd(
+                        interstitialAd.buildLoadAdConfig()
+                                .withAdListener(interstitialAdListener)
+                                .build());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ShowFullAds1() {
+        Log.e(TAG, "fbfull2 " + getString(R.string.fbfull1));
+        try {
+            if (inatboxtvapkindir5_splesh.interstitialAd2 != null && inatboxtvapkindir5_splesh.interstitialAd2.isAdLoaded())
+                inatboxtvapkindir5_splesh.interstitialAd2.show();
+            else {
+                if (!inatboxtvapkindir5_splesh.interstitialAd2.isAdLoaded())
+                    inatboxtvapkindir5_splesh.interstitialAd2.loadAd();
+
+                interstitialAd = new InterstitialAd(this, getString(R.string.fbfull1));
+                InterstitialAdListener interstitialAdListener = new InterstitialAdListener() {
+
+
+                    @Override
+                    public void onInterstitialDisplayed(Ad ad) {
+
+                    }
+
+                    @Override
+                    public void onInterstitialDismissed(Ad ad) {
+                        // Interstitial dismissed callback
+                        Log.e(TAG, "fbfull2 " + "Interstitial ad dismissed.");
+                    }
+
+                    @Override
+                    public void onError(Ad ad, AdError adError) {
+                        // Ad error callback
+                        Log.e(TAG, "fbfull2 " + adError.getErrorMessage());
+
+                    }
+
+                    @Override
+                    public void onAdLoaded(Ad ad) {
+                        Log.d(TAG, "fbfull2 " + "Interstitial ad is loaded and ready to be diSplash_screenlayed!");
+                        if (interstitialAd != null && interstitialAd.isAdLoaded())
+                            interstitialAd.show();
+                    }
+
+                    @Override
+                    public void onAdClicked(Ad ad) {
+                        // Ad clicked callback
+                        Log.d(TAG, "fbfull2 " + "Interstitial ad clicked!");
+                    }
+
+                    @Override
+                    public void onLoggingImpression(Ad ad) {
+                        // Ad impression logged callback
+                        Log.d(TAG, "fbfull2 " + "Interstitial ad impression logged!");
+                    }
+                };
+
+                interstitialAd.loadAd(
+                        interstitialAd.buildLoadAdConfig()
+                                .withAdListener(interstitialAdListener)
+                                .build());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
